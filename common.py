ca5861d6ee8d55da38e1b1f214ca795dff276dde
mohamedbouhali <153385561+mohamedbouhali@users.noreply.github.com>
2024-06-27 23:40:07 +0200

    Update common.py

--- common.py
+++ common.py
@@ -15,7 +15,7 @@ def get_args():
 	parser.add_argument('--learn', help='type of training method', type=str, default='normal', choices=learner_collection.keys())
 	
 
-	parser.add_argument('--env', help='gym env id', type=str, default='FetchReach-v1', choices=Robotics_envs_id)
+	parser.add_argument('--env', help='gym env id', type=str, default='FetchPush-v1', choices=Robotics_envs_id)
 	args, _ = parser.parse_known_args()
 	if args.env=='HandReach-v0':
 		parser.add_argument('--goal', help='method of goal generation', type=str, default='reach', choices=['vanilla', 'reach'])
